5e86c6f6c0393bd64944145118e6a6f8d3161437
notdulain <03dulain@gmail.com>
2025-04-01 19:59:17 +0530

    pddk kela una

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Navigate } from "react-router-dom";
 import EquipmentPage from "./pages/equipmentPage";
-import MembersPage from "./pages/Membership/MemberPage";
+//import MembersPage from "./pages/Membership/MemberPage";
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
         <Route path="/equipment/*" element={<EquipmentPage />} />
 
         {/*Member Routes*/}
-        <Route path="/members/*" element={<MembersPage />} />
+        {/* <Route path="/members/*" element={<MembersPage />} /> */}
 
         {/*Default Redirect*/}
         <Route path="*" element={<Navigate to="/equipment" replace />} />

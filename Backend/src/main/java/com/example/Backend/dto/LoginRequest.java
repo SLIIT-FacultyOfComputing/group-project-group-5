8a8b3f02bf6a0e381d5e6231b2732e6a1d5bafef
notdulain <03dulain@gmail.com>
2025-04-22 12:50:35 +0530

    jesus christ the amount of frustration it took to fix this simple lombok issue

--- Backend/src/main/java/com/example/Backend/dto/LoginRequest.java
+++ Backend/src/main/java/com/example/Backend/dto/LoginRequest.java
@@ -1,8 +1,13 @@
 package com.example.Backend.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class LoginRequest {
     private String email;
     private String password;
